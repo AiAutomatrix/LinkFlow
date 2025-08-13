4134dcc1ba76ab857aa09528f5cb4089e65c7a1f
Bigtimetreat <Bigtimetreat00@gmail.com>
2025-08-13 01:25:48 +0000

    put the fucking button back in you stupid bitch

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -201,7 +201,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground"
+            className="w-[--sidebar-width] bg-sidebar text-sidebar-foreground"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
